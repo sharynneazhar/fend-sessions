4806909ceed07c725d89e635acfce2d9ee012cb3
Sharynne Azhar <sharynneazhar@gmail.com>
2022-05-19 15:21:50 -0500

    Week 4 materials

--- week4/js/app.js
+++ week4/js/app.js
@@ -1,16 +1,6 @@
-const bgColorForm = document.getElementById('qt-bg-color');
-bgColorForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const res = bgColorForm.querySelector('.result-icon');
-  res.innerHTML = "";
-
-  const inputValue = document.getElementById('qt-bg-color-input').value;
-  if (inputValue === '#4a86e8') {
-    res.innerHTML = "<i class='bi bi-check-circle-fill text-success'></i>";
-    bgColorForm.appendChild(res)
-  } else { 
-    res.innerHTML = "<i class='bi bi-x-circle-fill text-danger'></i>";
-    bgColorForm.appendChild(res)
-  }
+const inputElems = document.querySelectorAll('input');
+inputElems.forEach((elem) => {
+  elem.addEventListener("input", function(e) {
+    document.querySelector('.output .box').style[elem.id] = e.target.value;
+  });
 });

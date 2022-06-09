6184715ec6bd287f95c0baead977e409f5ee1d82
Sharynne Azhar <sharynneazhar@gmail.com>
2022-06-09 21:12:57 +0000

    Updates

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>

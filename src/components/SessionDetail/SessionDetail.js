005bb0b75cdd3176092ce09841d42c93fcf3b6b5
Sharynne Azhar <sharynneazhar@gmail.com>
2022-10-29 20:19:40 +0000

    Update link

--- src/components/SessionDetail/SessionDetail.js
+++ src/components/SessionDetail/SessionDetail.js
@@ -2,18 +2,21 @@ import React from "react";
 import "./SessionDetail.css";
 
 function SessionDetail() {
-  const clickToCopy = () => {
-    const copyText = "https://udacityteam.zoom.us/my/sharynne.azhar";
-    navigator.clipboard.writeText(copyText);
-    alert("Link copied to clipboard");
-  };
-
   return (
     <div className="session-detail">
       <h2>Session Details</h2>
       <p>Sat, Sun, Wed</p>
       <p>8-10PM EASTERN</p>
-      <button onClick={clickToCopy}>Zoom Link</button>
+      <button
+        onClick={() => {
+          navigator.clipboard.writeText(
+            "https://udacityteam.zoom.us/my/sharynne.azhar"
+          );
+          alert("Link copied to clipboard");
+        }}
+      >
+        Zoom Link
+      </button>
     </div>
   );
 }
